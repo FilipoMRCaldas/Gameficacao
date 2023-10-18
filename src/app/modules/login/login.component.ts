04d4634b129d1722680a0a8215d5b9a19200f3b1
Filipo Caldas <filipo.caldas@sempreceub.com>
2023-10-17 21:17:48 -0300

    third commit

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  registration: string = '';
+  password: string = '';
 
+  login() {
+    // Implemente a lógica de autenticação aqui, por exemplo, verificar as credenciais em um servidor.
+    if (this.registration === 'usuario' && this.password === 'senha') {
+      // Login bem-sucedido, você pode redirecionar o usuário para a próxima página.
+      // Para navegação, você pode usar o serviço de roteamento do Angular.
+      // Exemplo: import { Router } from '@angular/router'; e usar o Router.navigate().
+    } else {
+      alert('Credenciais inválidas. Tente novamente.');
+    }
+  }
 }
